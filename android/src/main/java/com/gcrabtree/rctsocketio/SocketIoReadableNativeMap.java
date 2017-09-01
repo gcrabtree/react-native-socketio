2196c3927a6fcf4319b1e92adfc11df09590d914
Sal Principato <sal.principato@gmail.com>
2017-09-01 18:59:16 -0400

    Update SocketIoReadableNativeMap.java

--- android/src/main/java/com/gcrabtree/rctsocketio/SocketIoReadableNativeMap.java
+++ android/src/main/java/com/gcrabtree/rctsocketio/SocketIoReadableNativeMap.java
@@ -73,7 +73,7 @@ public class SocketIoReadableNativeMap extends ReadableNativeMap {
             String key = iterator.nextKey().toLowerCase();
             switch (key) {
                 case "force new connection":
-                case "force new"
+                case "forcenew"
                     opts.forceNew = options.getBoolean(key);
                     break;
                 case "multiplex":
