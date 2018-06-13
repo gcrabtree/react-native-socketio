2a9e810d5d95b41ed36aabf2264c370cfa79fa38
Saalihou Ndiaye <saalihou@gmail.com>
2018-06-13 01:23:54 +0000

    fix: Make library compatible with RN 0.29+
    ReactPackage no longer includes a createJSModules function

--- android/src/main/java/com/gcrabtree/rctsocketio/SocketIoPackage.java
+++ android/src/main/java/com/gcrabtree/rctsocketio/SocketIoPackage.java
@@ -25,7 +25,6 @@ public class SocketIoPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
