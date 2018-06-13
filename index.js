7e39485f6a9a2141a236e22da29928f7886a9d00
Saalihou Ndiaye <saalihou@gmail.com>
2018-06-13 01:45:57 +0000

    feat: Allow multiple event handlers on the same event

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ class Socket {
       // event parameters are emitted as an array
       // we spread them to keep the same interface
       // as the JS socketio client
-      this.handlers[event.name](
-        ...(event.hasOwnProperty('items')) ? event.items : [null]
+      this.handlers[event.name].map(handler =>
+        handler(...(event.hasOwnProperty("items") ? event.items : [null]))
       );
     }
     if (this.defaultHandlers.hasOwnProperty(event.name)) {
@@ -56,7 +56,8 @@ class Socket {
   }
 
   on (event, handler) {
-    this.handlers[event] = handler;
+    this.handlers[event] = this.handlers[event] || [];
+    this.handlers[event].push(handler);
     if (Platform.OS === 'android') {
       this.sockets.on(event);
     }
